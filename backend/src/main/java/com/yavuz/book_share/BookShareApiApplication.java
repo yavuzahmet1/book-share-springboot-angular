507f705b25b9eaf2e8e107412fcac3497021f95b
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-09 23:22:14 +0300

    init: setup Spring Boot backend for BookShare platform

---
+++ backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
@@ -0,0 +1,13 @@
+package com.yavuz.book_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShareApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yavuz/book_share/BookShareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yavuz.book_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
