1a54c1e0f4f20336c97f02f9abfa2eb9222b930c
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-11 22:04:28 +0300

    feat(security): add Token entity for email activation and user verification

---
+++ backend/src/main/java/com/yavuz/book_share/user/Token.java
@@ -0,0 +1,36 @@
+package com.yavuz.book_share.user;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdDate;
+    private LocalDateTime expiredDate;
+    private LocalDateTime validateDate;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
