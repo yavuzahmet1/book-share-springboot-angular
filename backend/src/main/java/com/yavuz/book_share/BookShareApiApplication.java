d07716e1492a8898d622a6c84c7f10ea19ea1807
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-11 22:03:12 +0300

    chore: activate JPA auditing for automatic entity timestamping

--- backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
+++ backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
@@ -2,8 +2,10 @@ package com.yavuz.book_share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookShareApiApplication {
 
 	public static void main(String[] args) {
