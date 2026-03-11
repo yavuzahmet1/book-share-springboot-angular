a4bcabf3ca2fc7ba578a81f30757b18c8f69bcc8
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-11 22:04:51 +0300

    feat(persistence): add TokenRepository with findByToken query method

---
+++ backend/src/main/java/com/yavuz/book_share/user/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.yavuz.book_share.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+
+}
