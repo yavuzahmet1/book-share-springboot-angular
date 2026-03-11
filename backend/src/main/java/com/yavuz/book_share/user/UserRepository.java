c399ea80eee2d69385af8e603085a85febd8278b
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-11 22:05:34 +0300

    feat(persistence): add UserRepository with findByEmail query method

---
+++ backend/src/main/java/com/yavuz/book_share/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yavuz.book_share.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+}
