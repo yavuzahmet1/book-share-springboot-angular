6584b495f8e98825bb81bc71ec25f01461798c58
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-13 22:22:00 +0300

    feat(security): configure authentication provider and password encoder
    - add BeansConfig class
    - configure DaoAuthenticationProvider with UserDetailsService
    - register BCryptPasswordEncoder bean

---
+++ backend/src/main/java/com/yavuz/book_share/config/BeansConfig.java
@@ -0,0 +1,31 @@
+package com.yavuz.book_share.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeansConfig {
+    private final UserDetailsService userDetailsService;
+
+    public BeansConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
