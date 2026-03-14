b68992592ccb81065a888e66bc46e2a4ff615fea
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-14 23:47:40 +0300

    feat(security): implement UserDetailsService for loading users by email

---
+++ backend/src/main/java/com/yavuz/book_share/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.yavuz.book_share.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yavuz.book_share.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + userEmail));
+    }
+
+}
