059df2ff58e2bacc9e9f0287190c1f8b5bec571e
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-15 17:29:40 +0300

    feat(mail): implement email service with thymeleaf template support

---
+++ backend/src/main/java/com/yavuz/book_share/auth/EmailService.java
@@ -0,0 +1,70 @@
+package com.yavuz.book_share.auth;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(
+            String to,
+            String username,
+            EmailTemplateName emailTemplate,
+            String confirmationUrl,
+            String activationCode,
+            String subject) {
+
+        String templateName;
+        if (emailTemplate == null) {
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplate.name();
+        }
+
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage,
+                    MULTIPART_MODE_MIXED_RELATED,
+                    UTF_8.name());
+
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("username", username);
+            properties.put("confirmationUrl", confirmationUrl);
+            properties.put("activationCode", activationCode);
+
+            Context thymeleafContext = new Context();
+            thymeleafContext.setVariables(properties);
+
+            helper.setFrom("contact@yavuz.com");
+            helper.setTo(to);
+            helper.setSubject(subject);
+
+            String template = templateEngine.process(templateName, thymeleafContext);
+            helper.setText(template, true);
+
+            mailSender.send(mimeMessage);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+}
