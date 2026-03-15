387cd69b90a93bb17b3ecf6faeda11cf159013bb
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-15 17:28:44 +0300

    feat(auth): create authentication controller with register endpoint

---
+++ backend/src/main/java/com/yavuz/book_share/auth/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.yavuz.book_share.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Endpoints for user authentication and registration")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegistirationRequest request) {
+        authenticationService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User registered successfully");
+    }
+
+}
