9cf08be1c28ff275aa5c6ec3725f23148e6541ae
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-15 17:29:11 +0300

    feat(auth): add user registration service with activation token generation

---
+++ backend/src/main/java/com/yavuz/book_share/auth/AuthenticationService.java
@@ -0,0 +1,73 @@
+package com.yavuz.book_share.auth;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.yavuz.book_share.role.RoleRepository;
+import com.yavuz.book_share.user.Token;
+import com.yavuz.book_share.user.TokenRepository;
+import com.yavuz.book_share.user.User;
+import com.yavuz.book_share.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+
+    public void register(RegistirationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("USER ROLE not found in the database"));
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdDate(LocalDateTime.now())
+                .expiredDate(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String chars = "0123456789";
+        StringBuilder token = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(chars.length());
+            token.append(chars.charAt(index));
+        }
+        return token.toString();
+    }
+
+}
