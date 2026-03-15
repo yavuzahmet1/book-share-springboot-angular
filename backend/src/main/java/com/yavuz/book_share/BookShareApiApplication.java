44cd84948e7d838d353252255e97b8f741583f8a
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-15 17:27:56 +0300

    feat(core): enable async processing

--- backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
+++ backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
@@ -3,9 +3,11 @@ package com.yavuz.book_share;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BookShareApiApplication {
 
 	public static void main(String[] args) {
