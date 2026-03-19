1579acfbd47d18e808e17961db8b47310ca5400a
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-19 23:38:28 +0300

    feat: add CommandLineRunner to initialize default USER role

--- backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
+++ backend/src/main/java/com/yavuz/book_share/BookShareApiApplication.java
@@ -1,9 +1,14 @@
 package com.yavuz.book_share;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.yavuz.book_share.role.Role;
+import com.yavuz.book_share.role.RoleRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,6 +17,18 @@ public class BookShareApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookShareApiApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build());
+
+			}
+		};
 	}
 
 }
