2ef4517870471b97e9f38d5522383a2fb67a335b
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-20 23:29:45 +0300

    refactor(security): change generateToken visibility to public

--- backend/src/main/java/com/yavuz/book_share/security/JwtService.java
+++ backend/src/main/java/com/yavuz/book_share/security/JwtService.java
@@ -29,7 +29,7 @@ public class JwtService {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    private String generateToken(
+    public String generateToken(
             Map<String, Object> claims,
             UserDetails userDetails) {
         return buildToken(claims, userDetails, jwtExpiration);
