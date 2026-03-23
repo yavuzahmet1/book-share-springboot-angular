9ab248bb3f8c40e6897bef5ee5e49553c388a78f
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-23 21:26:27 +0300

    feat(book): implement REST endpoints for book management
    - Add POST /books to save a new book
    - Add GET /books/{book-id} to retrieve a book by its ID
    - Add GET /books with pagination support for all displayable books
    - Integrate with BookService and Swagger documentation

---
+++ backend/src/main/java/com/yavuz/book_share/book/BookController.java
@@ -0,0 +1,45 @@
+package com.yavuz.book_share.book;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+@Tag(name = "Book", description = "Endpoints for managing books in the book sharing application")
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
+
+        return ResponseEntity.ok(bookService.save(request, connectedUser));
+    }
+
+    @GetMapping("{book-id}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok(bookService.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int pageSize,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(bookService.findAllBooks(page, pageSize, connectedUser));
+    }
+
+}
