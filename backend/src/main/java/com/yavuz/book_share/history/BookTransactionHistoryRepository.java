c125ce3177d5c2e87041ef1c4c1a99ea800a4f11
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-26 22:29:48 +0300

    feat(history): add query to fetch returned books by owner

--- backend/src/main/java/com/yavuz/book_share/history/BookTransactionHistoryRepository.java
+++ backend/src/main/java/com/yavuz/book_share/history/BookTransactionHistoryRepository.java
@@ -14,4 +14,11 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             """)
     Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
 
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.owner.id= :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+
 }
