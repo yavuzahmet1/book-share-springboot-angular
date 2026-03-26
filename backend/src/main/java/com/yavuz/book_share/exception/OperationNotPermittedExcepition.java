f08710f8b51f57c5724d62b9bf757d519373d683
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-26 22:27:59 +0300

    feat(exception): add OperationNotPermittedException

---
+++ backend/src/main/java/com/yavuz/book_share/exception/OperationNotPermittedExcepition.java
@@ -0,0 +1,9 @@
+package com.yavuz.book_share.exception;
+
+public class OperationNotPermittedExcepition extends RuntimeException {
+
+    public OperationNotPermittedExcepition(String msg) {
+        super(msg);
+    }
+
+}
