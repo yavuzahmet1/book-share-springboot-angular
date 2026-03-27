7420ac7199f732e8c169065f3a4a80da2fd85ade
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-27 22:58:48 +0300

    feat(book): add endpoints for archiving and borrowing books
    - Add PATCH `/books/archived/{book-id}` to update the archived status of a book
    - Add POST `/books/borrow/{book-id}` to allow users to borrow a book

--- backend/src/main/java/com/yavuz/book_share/book/BookController.java
+++ backend/src/main/java/com/yavuz/book_share/book/BookController.java
@@ -73,4 +73,18 @@ public class BookController {
         return ResponseEntity.ok(bookService.updateShareableStatus(bookId, connectedUser));
     }
 
+    @PatchMapping("/archived/{book-id}")
+    public ResponseEntity<Integer> updateArchivedStatus(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(bookService.updateArchivedStatus(bookId, connectedUser));
+    }
+
+    @PostMapping("/borrow/{book-id}")
+    public ResponseEntity<Integer> borrowBook(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(bookService.borrowBook(bookId, connectedUser));
+    }
+
 }
