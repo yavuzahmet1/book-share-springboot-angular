b3b0b7fb513b9a04b1dfccdf1a116dd198c95eb6
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-28 21:26:14 +0300

    feat(book): add endpoint to return borrowed books
    - Added PATCH mapping to `/borrow/return/{book-id}` in BookController
    - Integrated book return functionality with BookService

--- backend/src/main/java/com/yavuz/book_share/book/BookController.java
+++ backend/src/main/java/com/yavuz/book_share/book/BookController.java
@@ -87,4 +87,11 @@ public class BookController {
         return ResponseEntity.ok(bookService.borrowBook(bookId, connectedUser));
     }
 
+    @PatchMapping("/borrow/return/{book-id}")
+    public ResponseEntity<Integer> returnBorrowBook(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(bookService.returnBorrowedBook(bookId, connectedUser));
+    }
+
 }
