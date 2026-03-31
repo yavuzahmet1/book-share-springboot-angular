a2d38d30663207a7541f480ae20b860ef71bd0ac
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-31 22:05:08 +0300

    feat(feedback): add controller, mapper, and repository for feedback creation

---
+++ backend/src/main/java/com/yavuz/book_share/feedback/FeedBackRepository.java
@@ -0,0 +1,7 @@
+package com.yavuz.book_share.feedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedBackRepository extends JpaRepository<FeedBack, Integer> {
+
+}
