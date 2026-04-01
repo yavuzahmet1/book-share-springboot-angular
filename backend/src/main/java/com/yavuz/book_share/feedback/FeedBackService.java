a8b876ad45bae7b2707f3f02ffb9d22a9857b9a4
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-01 22:17:41 +0300

    feat(feedback): add paginated retrieval of feedbacks by book ID
    - Added `findAllFeedBackByBook` method in FeedBackService.
    - Implemented pagination using `PageRequest` and `Pageable`.
    - Mapped fetched `FeedBack` entities to `FeedBackResponse` with connected user context.

--- backend/src/main/java/com/yavuz/book_share/feedback/FeedBackService.java
+++ backend/src/main/java/com/yavuz/book_share/feedback/FeedBackService.java
@@ -1,12 +1,17 @@
 package com.yavuz.book_share.feedback;
 
+import java.util.List;
 import java.util.Objects;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.yavuz.book_share.book.Book;
 import com.yavuz.book_share.book.BookRepository;
+import com.yavuz.book_share.book.PageResponse;
 import com.yavuz.book_share.exception.OperationNotPermittedExcepition;
 import com.yavuz.book_share.user.User;
 
@@ -36,5 +41,23 @@ public class FeedBackService {
         FeedBack feedBack = feedBackMapper.toFeedBack(request);
 
         return feedBackRepository.save(feedBack).getId();
+    }
+
+    public PageResponse<FeedBackResponse> findAllFeedBackByBook(Integer bookId, int page, int size,
+            Authentication connectedUser) {
+        Pageable pageAble = PageRequest.of(page, size);
+        User user = ((User) connectedUser.getPrincipal());
+        Page<FeedBack> feedbacks = feedBackRepository.findAllByBookId(bookId, pageAble);
+        List<FeedBackResponse> feedBackResponses = feedbacks.stream()
+                .map(f -> feedBackMapper.toFeedBackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedBackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast());
     }
 }
