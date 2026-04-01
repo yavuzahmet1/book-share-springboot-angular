edac3c5944961e35b29a6461a4e412a942c0e240
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-01 22:23:07 +0300

    feat(feedback): implement paginated feedback retrieval for books
    - Added custom `@Query` `findAllByBookId` in `FeedBackRepository` for pagination.
    - Created `FeedBackResponse` DTO to return feedback details safely.
    - Implemented `findAllFeedBackByBook` in `FeedBackService` to orchestrate data fetching and mapping.

--- backend/src/main/java/com/yavuz/book_share/feedback/FeedBackRepository.java
+++ backend/src/main/java/com/yavuz/book_share/feedback/FeedBackRepository.java
@@ -1,7 +1,17 @@
 package com.yavuz.book_share.feedback;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FeedBackRepository extends JpaRepository<FeedBack, Integer> {
+
+    @Query("""
+            SELECT feedBack
+            FROM FeedBack feedBack
+            WHERE feedBack.book.id=:bookId
+             """)
+    Page<FeedBack> findAllByBookId(Integer bookId, Pageable pageAble);
 
 }
