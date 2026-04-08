6e25fc77a50dc3fbdd852c497bb0ce5b6988db43
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-08 22:02:24 +0300

    feat: add book routes for main component in book.routes.ts

---
+++ frontend/src/app/modules/book/book.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Main } from './pages/main/main';
+
+export const bookRoutes: Routes = [
+    {
+        path: '',
+        component: Main
+    }
+];
