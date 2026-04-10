77a8fcbebdf9fdb67764e2050c66dd4691ee2e8c
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-10 22:21:54 +0300

    refactor(common): use proper auditing annotation in BaseEntity
    Update updatedBy field to use @LastModifiedBy instead of @LastModifiedDate.

--- backend/src/main/java/com/yavuz/book_share/common/BaseEntity.java
+++ backend/src/main/java/com/yavuz/book_share/common/BaseEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -43,7 +44,7 @@ public class BaseEntity {
     @Column(updatable = false, nullable = false)
     private Integer createdBy;
 
-    @LastModifiedDate
+    @LastModifiedBy
     @Column(insertable = false)
     private Integer updatedBy;
 }
