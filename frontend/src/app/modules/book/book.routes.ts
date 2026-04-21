5b356e5921c623c5176ced283efc04dc4292fecd
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-21 23:09:39 +0300

    feat(routing): add manage book routes for create and edit
    - Import ManageBook component.
    - Add '/manage' route for creating new books.
    - Add '/manage/:bookId' route for editing existing books.

--- frontend/src/app/modules/book/book.routes.ts
+++ frontend/src/app/modules/book/book.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Main } from './pages/main/main';
 import { BookList } from './pages/book-list/book-list';
 import { MyBooks } from './pages/my-books/my-books';
+import { ManageBook } from './pages/manage-book/manage-book';
 
 export const bookRoutes: Routes = [
     {
@@ -20,6 +21,14 @@ export const bookRoutes: Routes = [
             {
                 path: 'my-books',
                 component: MyBooks,
+            }
+            , {
+                path: "manage",
+                component: ManageBook
+            }
+            , {
+                path: "manage/:bookId",
+                component: ManageBook
             }
         ]
     }
