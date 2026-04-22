56c582b08036135914daeb6e2a87e3d1e5aaf1e1
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-22 22:50:12 +0300

    feat(manage-book): implement book saving and cover upload logic
    - Added BookRequest model to manage form data state.
    - Injected BookService and Router for API communication and navigation.
    - Implemented async saveBook() method to handle saving the book and uploading the cover image sequentially.
    - Added error handling and success redirection to the "my-books" page.

--- frontend/src/app/modules/book/pages/manage-book/manage-book.ts
+++ frontend/src/app/modules/book/pages/manage-book/manage-book.ts
@@ -1,20 +1,37 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { BookRequest } from '../../../../services/models';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterModule } from "@angular/router";
+import { BookService } from '../../../../services/services';
+
 
 @Component({
   selector: 'app-manage-book',
-  imports: [],
+  imports: [FormsModule, RouterLink, RouterModule],
   templateUrl: './manage-book.html',
   styleUrl: './manage-book.scss',
 })
 export class ManageBook {
 
+  bookRequest: BookRequest = {
+    authorName: '',
+    isbn: '',
+    shareable: false,
+    synopsis: '',
+    title: ''
+  };
   errorMessage: Array<string> = [];
   selectedPicture: string | undefined;
   selectedBookCover: File | undefined;
-  constructor(private cdr: ChangeDetectorRef) { }
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private bookService: BookService,
+    private router: Router
+  ) { }
+
   onFileSelected(event: Event) {
     this.selectedBookCover = (event.target as HTMLInputElement).files?.[0];
-    console.log(this.selectedBookCover);
     if (this.selectedBookCover) {
       const reader = new FileReader();
       reader.onload = (e) => {
@@ -22,6 +39,27 @@ export class ManageBook {
         this.cdr.detectChanges();
       };
       reader.readAsDataURL(this.selectedBookCover);
+    }
+  }
+
+  async saveBook() {
+    try {
+      const bookId = await this.bookService.saveBook({
+        body: this.bookRequest
+      });
+
+      if (this.selectedBookCover) {
+        await this.bookService.uploadBookCoverPicture({
+          'book-id': bookId,
+          body: { file: this.selectedBookCover }
+        });
+      }
+
+      this.router.navigate(['/books/my-books']);
+
+    } catch (err: any) {
+      this.errorMessage =
+        err?.error?.validationErrors || ['Error saving book. Please try again later.'];
     }
   }
 
