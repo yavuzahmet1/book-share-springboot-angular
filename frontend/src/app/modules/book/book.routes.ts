bb0acbda8e31109dc8e76024c9ba1c0415023482
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-04-24 22:34:28 +0300

    feat(routing): add route for borrowed books list
    - Imported BorrowedBookList component
    - Added 'my-borrowed-books' path to main routes

--- frontend/src/app/modules/book/book.routes.ts
+++ frontend/src/app/modules/book/book.routes.ts
@@ -3,6 +3,7 @@ import { Main } from './pages/main/main';
 import { BookList } from './pages/book-list/book-list';
 import { MyBooks } from './pages/my-books/my-books';
 import { ManageBook } from './pages/manage-book/manage-book';
+import { BorrowedBookList } from './pages/borrowed-book-list/borrowed-book-list';
 
 export const bookRoutes: Routes = [
     {
@@ -21,6 +22,11 @@ export const bookRoutes: Routes = [
             {
                 path: 'my-books',
                 component: MyBooks,
+            }
+            ,
+            {
+                path: 'my-borrowed-books',
+                component: BorrowedBookList,
             }
             , {
                 path: "manage",
